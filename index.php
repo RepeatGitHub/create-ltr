d9e10016b921213d9c6e794c26db8c21b643fc3d
Repeat <48736123+RepeatGitHub@users.noreply.github.com>
2022-08-09 16:42:39 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en" class="notranslate" translate="no">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=0.8">
+		<meta name="keywords" content="SethBling,Loot,Table,Randomizer,Web,Version">
+		<link rel="shortcut icon" href="style/icon_randomizer.gif" type="image/gif" />
+		<link rel="icon" href="style/icon_randomizer.gif" type="image/gif" />
+		<link rel="stylesheet" type="text/css" media="screen" href="style/minecraft-webfont.css?v=1617542206" />
+		<!--[if IE]>
+			<div id="the_big_middle_finger">
+				<link rel="stylesheet" href="style/ie_too_old.css" />
+				<h1>Hold your bees there, buddy!</h1>
+				<h3>The randomizer might not work properly in your browser.</h3>
+				<h3>Do us all a favor and update your Browser.</h3>
+				You can get an alternative here:<br>
+				<a href="https://brave.com/fas623">Brave (Referral Link)</a><br>
+				<a href="https://www.google.com/intl/en_us/chrome/">Google Chrome</a><br>
+				<a href="https://www.mozilla.org/de/firefox/">Mozilla Firefox</a><br>
+				<a href="https://www.opera.com/">Opera</a><br>
+			</div>
+		<![endif]-->
+		<!--[if !IE]><!-->
+			<link rel="stylesheet" href="style/style.css?v=1617542206" />
+		<!--[endif]-->
+		<title>Minecraft Create Mod Randomizer - Menu</title>
+		<script src="dist/app.bundle.js?v=2" type="module" defer></script>
+	</head>
+	<body>
+		<div id="border_shadow"></div>
+		<label id="main_header">Minecraft Create Mod Randomizer by Repeat</label>
+		<div id="page_wrapper">
+			<input type="radio" class="submenu" name="submenus" id="sub_main" checked/>
+			<input type="radio" class="submenu" name="submenus" id="sub_credits"/>
+			<input type="radio" class="submenu" name="submenus" id="sub_changelog"/>
+			<span id="menu_header"></span><br>
+			<div id="menu">
+				<!-- Main Menu -->
+				<div class="menu_in">
+					<div id="content" class="submenu_content">
+
+						<div id="progress_report" class="center">
+							<h1 id="title">Generating files...</h1>
+							<div id="progress_reporter"></div>
+							<div class="textbox_container mc_tooltip">
+								<span class="mc_tooltiptext">This is the randomization seed, that was used in this session<p style="color:yellow">You can save it, to get the same output again when reusing the randomizer</p></span>
+								<label>Your Randomization Seed:</label>
+								<input class="textbox" type="text" autocomplete="off" id="progress_seed" readonly/>
+							</div>
+							<br>
+							<div class="button_container">
+								<a href="#" id="back_button" class="button_link option">Back to Randomization Menu</a>
+							</div>
+						</div>
+
+						<div id="selection_menu">
+							<div class="dropdown_container">
+								<label>Minecraft Version:</label>
+								<select id="version">
+									<optgroup label="Java Edition">
+										<option value="1.18.X">(J) Create 0.5</option>
+									</optgroup>
+								</select>
+							</div>
+							<!-- Randomization Seed Textbox -->
+							<div class="textbox_container mc_tooltip">
+								<span class="mc_tooltiptext">This sets the seed at which the randomization starts. It does NOT have to be the same seed as your world.<p class="mc_yellow">Only the characters A-Z, a-z and 0-9 are supported.</p></span>
+								<label>Randomization Seed:</label>
+								<input type="text" maxlength="20" pattern="[a-zA-Z0-9-]+" autocomplete="off" min="-9223372036854775808" max="9223372036854775807" id="seed" value=""/>
+							</div>
+							<div class="options">
+								<hr>
+								<!-- Randomize Datapack -->
+								<div>
+									<input type="checkbox" id="menu_datapack" class="menu_checkbox" checked/>
+									<div class="mc_tooltip menu_switch_pass">
+										<label class="menu_switch" for="menu_datapack">Randomize Datapack <div class="status"></div></label>
+										<span class="mc_tooltiptext">
+											Should a datapack be generated?
+											<p class="mc_dark_red">
+												You need to have access to the map files to use the generated datapack
+											</p><br>
+											Compatibility:<br>
+											1.14.X - 1.19.X <span class="mc_green">✓</span><br>
+											1.13.X <span class="mc_yellow">✓</span><br>
+										</span>
+									</div>
+									<div class="menu">
+										<div class="options">
+											<div class="submenu_header">
+												Loot Table
+											</div>
+											<!-- Randomize Blocks Checkbox -->
+											<div class="button_container mc_tooltip">
+												<span class="mc_tooltiptext">Randomize block-drops<br><p class="mc_red">Not supported in (J) 1.13.X</p></span>
+												<label class="option">
+													<input type="checkbox" id="randomizationOptions[datapack][lt_blocks]" checked/>
+													Randomize Blocks:
+													<span class="state"></span>
+												</label>
+											</div>
+											<!-- Randomize Chests Checkbox -->
+											<div class="button_container mc_tooltip">
+												<span class="mc_tooltiptext">Randomize items inside generated chests<p class="mc_yellow">Chests in Dungeons, Mineshafts, etc.</p></span>
+												<label class="option">
+													<input type="checkbox" id="randomizationOptions[datapack][lt_chests]" checked/>
+													Randomize Chests:
+													<span class="state"></span>
+												</label>
+											</div>
+											<!-- Randomize Entites Checkbox -->
+											<div class="button_container mc_tooltip">
+												<span class="mc_tooltiptext">Randomize mob drops</span>
+												<label class="option">
+													<input type="checkbox" id="randomizationOptions[datapack][lt_entities]" checked/>
+													Randomize Entities:
+													<span class="state"></span>
+												</label>
+											</div>
+											<!-- Randomize Gameplay Checkbox -->
+											<div class="button_container mc_tooltip">
+												<span class="mc_tooltiptext">Randomize special drops<p class="mc_yellow">Fishing, Hero of the Village Gifts, etc.</p></span>
+												<label class="option">
+													<input type="checkbox" id="randomizationOptions[datapack][lt_gameplay]" checked/>
+													Randomize Gameplay:
+													<span class="state"></span>
+												</label>
+											</div>
+											<div class="submenu_header">
+												Crafting
+											</div>
+											<!-- Randomize Crafting Recipes Checkbox -->
+											<div class="button_container mc_tooltip">
+												<span class="mc_tooltiptext">Randomize crafting recipes.</span>
+												<label class="option">
+													<input type="checkbox" id="randomizationOptions[datapack][cr_all]"/>
+													Randomize Crafting Recipes:
+													<span class="state"></span>
+												</label>
+											</div>
+										</div>
+									</div>
+								</div>
+								<hr>
+							</div>
+							<br>
+							
+							<!-- Randomize Button -->
+							<div class="button_container mc_tooltip">
+								<span class="mc_tooltiptext">Start randomizing your selections</span>
+								<input id="start_randomizer" class="button option" type="submit" value="Randomize"/>
+							</div>
+
+							<div class="information">
+								This randomizer is still in development and more features will be coming soon.
+								If you'd like to support the development, then here are some ways you can:
+								
+								<a href="https://fasguy.net/discord">Join my Discord Server</a> for feedback and questions
+								<a href="https://paypal.me/Fasguy" target="_blank">Donate using PayPal.me</a>
+							</div>
+						</div>
+					</div>
+					<div id="changelog" class="submenu_content mc_white">
+						<a href="https://github.com/Fasguy/MinecraftRandomizer" target="_blank" rel="noreferrer">Randomizer Version</a>
+						<div class="sclf_changelog"><div class="sclf_version"><span class="sclf_version_header ">Version c.1.0 (9.8.2022)
+</span><p class="sclf_addition mc_green">Copied Fasguy`s site. Lmao.
+</p><span class="sclf_version_header ">Version 2.4 (24.06.2020)
+</span><p class="sclf_addition mc_green">Added Loot-Table randomization support for Minecraft Java v1.16
+</p><p class="sclf_addition mc_green">Added Crafting Recipe randomization support for Minecraft Java v1.16
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 2.3 (17.02.2020)
+</span><p class="sclf_addition mc_green">Added Crafting Recipe randomization support for Minecraft Java v1.14
+</p><p class="sclf_addition mc_green">Added Loot-Table randomization support for Minecraft Java v1.13
+</p><p class="sclf_addition mc_green">Added Crafting Recipe randomization support for Minecraft Java v1.13
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 2.2.1 (20.12.2019)
+</span><p class="sclf_general mc_yellow">Fixed "Empty Randomizations" being possible again
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 2.2 (19.12.2019)
+</span><p class="sclf_addition mc_green">Added Crafting Recipe randomization support for Minecraft Java v1.15
+</p><p class="sclf_general mc_yellow">UI changes to the randomization page. (The page no longer reloads completely when updating progress)
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 2.1.1 (15.12.2019)
+</span><p class="sclf_general mc_yellow">Fixed broken loot tables in Minecraft Java v1.15 loot table datapack
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 2.1 (14.12.2019)
+</span><p class="sclf_addition mc_green">Added Loot-Table randomization support for Minecraft Java v1.15
+</p><p class="sclf_general mc_yellow">Updated UI to reflect Minecraft Java v1.15's UI more
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 2.0.1 (05.11.2019)
+</span><p class="sclf_general mc_yellow">Fixed "killed_by_player" condition reappearing
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 2.0 (01.11.2019)
+</span><p class="sclf_general mc_yellow">Shulkerboxes drop themselves
+</p><p class="sclf_general mc_yellow">Renamed to "Minecraft Randomizer"
+</p><p class="sclf_general mc_yellow">Full UI redesign
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 1.1.3 (15.08.2019)
+</span><p class="sclf_addition mc_green">Added information file into the zip file, telling people to not extract the zip file
+</p><p class="sclf_general mc_yellow">Blaze Rods are now obtainable
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 1.1.2 (03.08.2019)
+</span><p class="sclf_general mc_yellow">Small adjustments to prevent "empty" loot tables from being generated
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 1.1.1 (25.07.2019)
+</span><p class="sclf_addition mc_green">Added instructions based on feedback from hungryhyena78
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 1.1 (13.07.2019)
+</span><p class="sclf_addition mc_green">You can now choose which loot table types are being randomized
+</p><p class="sclf_removal mc_red">Reverted some of the changes for JavaScript detection, due to progress not always being shown when JavaScript is actually activated
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 1.0.3 (10.07.2019)
+</span><p class="sclf_general mc_yellow">The generator page should be way less aggressive now, when having JavaScript disabled
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 1.0.2 (10.07.2019)
+</span><p class="sclf_general mc_yellow">Made some adjustments to keep the server cleaner in case someone aborts the process (PHP Session file removal)
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 1.0.1 (09.07.2019)
+</span><p class="sclf_addition mc_green">Added proper styling to the website to make it a bit more pleasing to look at
+</p></div><div class="sclf_version"><span class="sclf_version_header ">Version 1.0 (08.07.2019)
+</span><p class="sclf_general mc_yellow">First Version of Loot Table randomizer by SethBling (PHP Port) released
+</p></div></div>					</div>
+					<div id="credits" class="submenu_content">
+						<div id="credits_text">
+							<p id="credits_header">
+								============
+								<span class="mc_yellow">Minecraft Create Mod Randomizer</span>
+								============
+							</p>
+							<p id="credits_main">
+								<span class="mc_grey">Create Randomizer Website Developer</span>
+								<span class="space">Repeat</span>
+								
+								<span class="mc_grey">Original Randomizer Website Developer</span>
+								<span class="space">Fasguy</span>
+								
+								<span class="mc_grey">Original LootTable Randomizer Developer</span>
+								<span class="space">SethBling</span>
+								
+								<span class="mc_grey">PclZip Developer</span>
+								<span class="space">Vincent</span>
+
+								<span class="mc_grey">Minecraft Developer Studio</span>
+								<span class="space">Mojang</span>
+								
+								<span class="mc_grey">Special Thanks</span>
+								<span class="space">hungryhyena78</span>
+								<span class="space">iBazly</span>
+								<span class="space">jesserules101</span>
+								<span class="space">Pixel</span>
+
+								<span class="mc_grey">Please send help</span>
+								<span class="space">Web development is a pain</span>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="selector_menu">
+				<label class="selector_button selector_button_first" for="sub_main">
+					<div class="sublogo" style="--icon:url(icon_randomizer.gif)"></div>
+				</label>
+				<label class="selector_button" for="sub_changelog">
+					<div class="sublogo" style="--icon:url(icon_changelog.png)"></div>
+				</label>
+				<label class="selector_button" for="sub_credits">
+					<div class="sublogo" style="--icon:url(icon_credits.png)"></div>
+				</label>
+			</div>
+			<footer id="footer">Minecraft © 2009 - 2022 Mojang.
+				I am not affiliated with Mojang or any other mentioned entities in any way.
+			</footer>
+		</div>
+	</body>
+</html>
